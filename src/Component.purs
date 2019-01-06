3d0b2ab0e45d2a137530e65fd0888546962a01b7
Hackle & Hincal <devtesthackleandhincal@trademe.co.nz>
2019-01-06 13:27:57 +1300

    can show markdown

--- src/Component.purs
+++ src/Component.purs
@@ -14,16 +14,22 @@ import Affjax.RequestHeader
 import Data.MediaType
 import Foreign.Object
 import Data.Argonaut.Core
+import Text.Markdown.SlamDown.Halogen.Component
+import Text.Markdown.SlamDown.Parser
+import Data.Foldable (for_)
 
 data Query a = Init a
+                -- | ChangeDocument String a
 
 type State = { title :: String, content :: String }
 
 data SlamDownSlot = SlamDownSlot
+derive instance slamDownSlotEq :: Eq SlamDownSlot
+derive instance slamDownSlotOrd :: Ord SlamDownSlot
 
 component :: H.Component HH.HTML Query Unit Void Aff
 component =
-    H.component
+    H.parentComponent
         { initialState: const initialState
         , render
         , eval
@@ -34,13 +40,13 @@ component =
     initialState :: State
     initialState = { title: "Loading", content: "Loading" }
 
-    render :: State -> H.ComponentHTML Query
+    render :: State -> H.ParentHTML Query (SlamDownQuery String) SlamDownSlot Aff
     render state =
         HH.div_
         [ HH.h1_
             [ HH.text state.title ]
-        -- , HH.div_
-        --     [ HH.slot SlamDownSlot (slamDownComponent state.content config) unit Void ]
+        , HH.div_
+            [ HH.slot SlamDownSlot slamDownComponent unit absurd ]
         ]
     
     req :: AX.Request Json
@@ -50,14 +56,16 @@ component =
                             , headers = [ Accept (MediaType "text/markdown") ]
                             }
 
-    eval :: Query ~> H.ComponentDSL State Query Void Aff
-    eval = case _ of
+    eval :: Query ~> H.ParentDSL State Query (SlamDownQuery String) SlamDownSlot Void Aff
+    eval q = case q of
         Init next -> do
             response <- H.liftAff $ AX.request req
             case response.body of
                 Left _ -> pure next
-                Right resp -> do
-                    _ <- H.modify (\state -> decodeState resp)
+                Right raw -> do
+                    let resp = decodeState raw
+                    _ <- H.modify (\state -> resp)
+                    for_ (parseMd resp.content) \md -> H.query SlamDownSlot (H.action (SetDocument md))
                     pure next
 
     decodeState :: Json -> State
